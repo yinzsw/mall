517aa4c5e15a3bdf37c5d01e10a54a8fcbd98f2e
yinzs <yinzsw@qq.com>
2021-07-17 10:32:07 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import toast from 'components/common/toast/'
+import VueLazyload from "vue-lazyload";
+import Toast from 'components/common/toast/'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
-Vue.use(toast)
 
+Vue.use(Toast)
+Vue.use(VueLazyload, {
+  loading: 'data:image/gif;base64,R0lGODlhAQABAPAAAObm5gAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
+})
+
+fastClick.attach(document.body)
 new Vue({
   router,
   store,
